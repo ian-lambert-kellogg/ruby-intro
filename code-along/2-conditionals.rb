821bb6e91f7b6e5f5b00e1c9ce03a2a92a87aee2
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-01-30 01:02:10 +0000

    completed data and conditionals

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -1,15 +1,65 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 2-conditionals.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 2-conditionals.rb
 
-# Booleans
+# # Booleans
 
-# Boolean Expressions
+# test_is_true = true
+# puts test_is_true
 
-# If Conditional Logic
+# test_is_false = false
+# puts test_is_false
+
+
+
+# # Boolean Expressions
+
+# puts 3 == 2
+
+# puts 3 != 2
+
+# puts 3 > 2
+
+# puts 3 < 2
+
+# # If Conditional Logic
+
+# if 3 == 2 
+#     puts "This text should never display"
+# end 
+
+# if 3 > 2 
+#     puts "This text should always display"
+# end 
 
 # If/Else Conditional Logic
 
+if 3 == 2
+    puts "this text should nevers display"
+else
+    puts "this text should always appear"
+end 
+
+
 # Elsif Conditional Logic
 
+user_entered_password = "tacos"
+
+real_password = "secret"
+
+if user_entered_password == real_password
+    puts "wrong password. Try again"
+end
+
+
+
+
 # Combining Expressions
+
+temp = 68
+precipitation = 0
+
+if temp >= 65 && temp <= 75 && precipitation == 0
+    puts "it's perfect outside!"
+end 
+

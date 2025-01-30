06a42d1ee499811d66273c8bc78260253c2f2822
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-01-30 02:20:59 +0000

    made some changes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,35 @@
 
 # Hashes are lists of *key-value pairs*
 
+ben = { "name" => "Ben", "location" => "Chicago", "status" => "teaches at Kellogg"}
+
+
+
+
+
 # Accessing data from the hash
 
+
+
+name = ben["name"]
+puts name
+
+location = ben["location"]
+puts location
+
+
+
 # More Complex Hashes
+
+
+my_porfile = {
+
+    "name" => "Ben"
+    "location" => {"city" => "Chicago" , "state" => "IL"},
+    "status" => "teacher"
+}
+
+
+
+p my_porfile
+puts 
